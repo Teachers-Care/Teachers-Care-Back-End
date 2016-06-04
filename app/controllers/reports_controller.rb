b4e8316b296e219c95c5456717559aa82281f49e
Jennifer Ford <jenncford@gmail.com>
2016-06-04 18:46:33 -0400

    create methode for reports

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,17 @@
+class ReportsController < ApplicationController
+  def create
+  @report = Report.new(report_params)
+  if @report.save
+    render "create.json.jbuilder", status: :created
+  else
+    render json: { errors: @report.errors.full_messages },
+           status: :unprocessable_entity
+  end
+  end
+  private
+  def report_params
+    params.permit(:dating, :travel, :runaway, :homeless, :tatoo, :truant, :luxury, :provocative,
+    :explicit_online, :sti, :pregnancy, :cps, :comments)
+  end
+
+end
