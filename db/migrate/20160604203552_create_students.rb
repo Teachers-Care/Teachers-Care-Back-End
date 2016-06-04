33ed40cdbca97da3e55bdc2fb1cfab19547e1cb3
Jennifer Ford <jenncford@gmail.com>
2016-06-04 16:51:34 -0400

    Student table

--- db/migrate/20160604203552_create_students.rb
+++ db/migrate/20160604203552_create_students.rb
@@ -1,6 +1,13 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
+      t.string :first
+      t.string :last
+      t.integer :student_number
+      t.integer :grade
+      t.integer :age
+      t.boolean :female, default: false
+      t.boolean :male, default: false
 
       t.timestamps null: false
     end
