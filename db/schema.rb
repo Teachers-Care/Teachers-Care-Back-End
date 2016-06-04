5e88aff9b807135b7de776870b6e53c5597ed7a4
Jennifer Ford <jenncford@gmail.com>
2016-06-04 17:18:01 -0400

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604205405) do
+ActiveRecord::Schema.define(version: 20160604211053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20160604205405) do
     t.boolean  "pregnancy",       default: false
     t.boolean  "cps",             default: false
     t.string   "comments"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  create_table "staffs", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
   end
