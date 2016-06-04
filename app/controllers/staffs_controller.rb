0b5852353a501b402f2cafaf13f017401edf3ff4
Jennifer Ford <jenncford@gmail.com>
2016-06-04 18:08:11 -0400

    create method for staff

---
+++ app/controllers/staffs_controller.rb
@@ -0,0 +1,15 @@
+class StaffsController < ApplicationController
+  def create
+  @staff = Staff.new(staff_params)
+  if @staff.save
+    render "create.json.jbuilder", status: :created
+  else
+    render json: { errors: @staff.errors.full_messages },
+           status: :unprocessable_entity
+  end
+  end
+  private
+  def staff_params
+    params.permit(:username, :password, :admin)
+  end
+end
