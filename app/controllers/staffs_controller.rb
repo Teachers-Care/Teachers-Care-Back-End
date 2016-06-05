7c944d85e4bff0b986dd4ff4af30b7307c1e4b7b
Jennifer Ford <jenncford@gmail.com>
2016-06-04 20:35:38 -0400

    create method works

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,6 +1,7 @@
 class StaffsController < ApplicationController
   def create
   @staff = Staff.new(staff_params)
+  binding.pry
   if @staff.save
     render "create.json.jbuilder", status: :created
   else
